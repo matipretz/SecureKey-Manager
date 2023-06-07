2ab7ed45b4164e132d0cfb668acee8bb61ea9837
Matias Martin Murad Pretz <30426732+matipretz@users.noreply.github.com>
2023-06-07 01:07:20 -0300

    Update Jfun.java

--- src/Jfun.java
+++ src/Jfun.java
@@ -9,7 +9,46 @@ public  class Jfun {
         print("###\nSecureKey Manager 0.1\nCaC 4.0-23438-2023 SEM1\nAlumno: Matias Martin Murad Pretz\n33.366.158\nDocente: Gonzalo F. Rubé\nTutora: Zoraida Flores\n###");
     }
     public static void menu() {
-        print("### MENU PRINCIPAL ###\nSeleccione una opcion:\n+1. Iniciar sesion.\n2. Crear usuario.\n3. Salir.");        
+        print("### MENU PRINCIPAL ###\nSeleccione una opcion:\n+1. Iniciar sesion.\n2. Crear usuario.\n3. Salir.");      
+        /* try:
+        choose = int(input('Enter choice:'))
+        except ValueError:
+        continue
+        if choose == 1:#iniciar sesion#
+            iniciarSesion
+        if choose == 2:#crear usuario#
+            crearUsuario
+        if choose == 3:#salir#
+            salir
+        default
+            err
+
+        def iniciarSesion
+            print ingrese usuario
+            in usuario
+            print ingrese pass
+            in pass
+                if ok
+                    menuInterno
+
+        def crear usuario
+            print nombre de usuario
+            in nombre de usuario
+            print pass
+            in pass
+            save()
+
+        def menu interno
+            print crearPass o modificarPass o eliminarPass o cerrarSesion
+
+        def crearPass
+        def modificarPass
+        def eliminarPass
+        def cerrarSesion
+        def salir
+        def save
+
+        */  
     }
 
 }
