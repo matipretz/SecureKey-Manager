f1b4e4829ee2e6bb10b02f9b0c237697ee8335cf
Matias Martin Murad Pretz <30426732+matipretz@users.noreply.github.com>
2023-06-07 00:56:00 -0300

    Update Jfun.java

--- src/Jfun.java
+++ src/Jfun.java
@@ -6,11 +6,10 @@ public  class Jfun {
     }
 
     public static void welcome() {
-        print("###\nSecureKey Manager 0.1\nCaC 4.0-23438-2023 SEM1\nAlumno: Murad Pretz Matias Martin\n33.366.158\nDocente: Rubé Gonzalo F.\nTutora:Gonzales Zoraida\n###");
+        print("###\nSecureKey Manager 0.1\nCaC 4.0-23438-2023 SEM1\nAlumno: Matias Martin Murad Pretz\n33.366.158\nDocente: Gonzalo F. Rubé\nTutora: Zoraida Flores\n###");
     }
     public static void menu() {
-        print("### MENU PRINCIPAL ###\nSeleccione una opcion:\n+1. Iniciar sesion.\n2. Crear usuario.\n3. Salir.");
-        
+        print("### MENU PRINCIPAL ###\nSeleccione una opcion:\n+1. Iniciar sesion.\n2. Crear usuario.\n3. Salir.");        
     }
 
 }
