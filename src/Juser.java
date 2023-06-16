45f66d78e86cb1cb9901d4c990b80d6805ea354b
Matias Martin Murad Pretz <30426732+matipretz@users.noreply.github.com>
2023-06-16 13:26:37 -0300

    fixes

--- src/Juser.java
+++ src/Juser.java
@@ -5,7 +5,7 @@ import java.io.PrintWriter;
 import java.util.Scanner;
 
 public class Juser {
-    public static final String FILE_PATH = "data/users"; // Ruta del archivo donde se guardarán las passwords
+    public static String FILE_PATH = "data/users"; // Ruta del archivo donde se guardarán las passwords
     
     static String nombre;
     static String contrasena;    
@@ -35,12 +35,12 @@ public class Juser {
             System.out.print(">> El nombre no puede estar vacío.\n>> Ingrese su nombre de usuario:\n>> ");
             user = scanner.nextLine();
             }
-        while (Jfun.verificar(user, null, FILE_PATH)) {
+        while (Jfun.verificar(user, null, Juser.FILE_PATH)) {
             Jfun.clear();
             System.out.print(">> El nombre de usuario ya está en uso.\n>> Ingrese su nombre de usuario:\n>> ");
-            user = scanner.nextLine();    
+            user = scanner.nextLine();
         }
-        try (PrintWriter writer = new PrintWriter(new FileWriter(FILE_PATH, true))) {
+        try (PrintWriter writer = new PrintWriter(new FileWriter(Juser.FILE_PATH, true))) {
             System.out.print(">> Ingrese su password:\n>> ");
             String password = scanner.nextLine();
             while (password.trim().isEmpty()) { // Validar que el campo name no esté vacío
@@ -50,6 +50,8 @@ public class Juser {
             Jfun.clear();
             System.out.println(">> Creando usuario...");
             Jfun.pausa(1500);
+            setNombre(user);
+            setContrasena(password);
             writer.println(user + ":" + password);
             System.out.println(">> Usuario creado.\nPresione [ENTER] para continuar.");
             Jmenus.mainMenu();
@@ -66,7 +68,7 @@ public class Juser {
         String user = scanner.nextLine();
         char[] passwordArray = console.readPassword(">> Ingrese su password:    (echo=off)\n>> ");
         String password = new String(passwordArray);
-        if (Jfun.verificar(user, password,FILE_PATH)) {
+        if (Jfun.verificar(user, password, Juser.FILE_PATH)) {
             Jfun.clear();
             System.out.println(">> Hola " + user + ".");
             setNombre(user);
