375f91997fc6067b408c4f7af320ffd22e677cdc
Matias Martin Murad Pretz <30426732+matipretz@users.noreply.github.com>
2023-06-28 22:28:33 -0300

    Update limpiar()

--- src/Varios.java
+++ src/Varios.java
@@ -74,9 +74,17 @@ class Varios {
         }
     }
 
-    static void limpiar() { // Limpia la consola.
-        System.out.print("\033[H\033[2J");
-        System.out.flush();
+    static void limpiar() {
+        try {
+            final String os = System.getProperty("os.name");
+            if (os.contains("Windows")) {
+                Runtime.getRuntime().exec("cls");
+            } else {
+                Runtime.getRuntime().exec("clear");
+            }
+        } catch (final Exception e) {
+            e.printStackTrace();
+        }
     }
 
     static String ajustar(String input) {
